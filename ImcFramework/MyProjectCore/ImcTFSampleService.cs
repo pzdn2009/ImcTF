e33de9f03f1118e13abd44a1ae23d6b2ef660334
pzdn2009 <1050244110@qq.com>
2016-07-19 00:04:12 +0800

    #add demo project

--- ImcFramework/MyProjectCore/ImcTFSampleService.cs
+++ ImcFramework/MyProjectCore/ImcTFSampleService.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Common.Logging;
+using ImcFramework.Core;
 using ImcFramework.WcfInterface;
 using Quartz;
 using System.Threading;
-using Common.Logging;
 
-namespace ImcFramework.Core.BuildInBusinessService
+namespace MyProjectCore
 {
     public class ImcTFSampleService : MainBusinessBase
     {
@@ -19,15 +15,6 @@ namespace ImcFramework.Core.BuildInBusinessService
                 return new EServiceType("ImcTFSample", "框架示例服务");
             }
         }
-
-        //public override void Execute(IJobExecutionContext context)
-        //{
-        //    NotifyAndLog("开始！", LogLevel.Info);
-
-        //    Thread.Sleep(2000);
-
-        //    NotifyAndLog("结束！", LogLevel.Info);
-        //}
 
         public override void ExecuteCore(IJobExecutionContext context)
         {
